7a18991aa399ec10797cc83d220bd1007967b88f
Puneet <codydeny@gmail.com>
2019-07-24 18:46:11 +0530

    first look

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+function NavShow(value) {
+	
+        switch(value) {
+
+        case 1:
+        $('#maincontent').load('home.html #main1');
+        break;
+
+         case 2:
+         $('#maincontent').load('resources.html #main2')
+        break;
+
+         case 3:
+         $('#maincontent').load('resources.html #main3')
+        break;
+
+         case 4:
+         $('#maincontent').load('resources.html #main4')
+        break;
+
+         case 5:
+         $('#maincontent').load('resources.html #main5')
+        break;
+
+         case 6:
+         $('#maincontent').load('resources.html #main6')
+        break;
+
+
+
+        }
+     
+	 
+}
+
