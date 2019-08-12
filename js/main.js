dd30e4895e2fae5377bee8feb28d8d8b74975b2b
Puneet <codydeny@gmail.com>
2019-08-12 18:27:27 +0530

    Updated

--- js/main.js
+++ js/main.js
@@ -13,15 +13,15 @@ function NavShow(value) {
         switch(value) {
 
         case 1:
-        $('#maincontent').load('home.html #main1');
+       //
         break;
 
          case 2:
-         $('#maincontent').load('resources.html #main2')
+         $('#maincontent').load('index.html #main2')
         break;
 
          case 3:
-         $('#maincontent').load('resources.html #main3')
+         $('#maincontent').load('index.html #main3')
         break;
 
          case 4:
